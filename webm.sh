d4a6d8bd698b8cbf9e2b16fddf94a0a8ef96589b
BaltiskayaGuberniya <110169821+BaltiskayaGuberniya@users.noreply.github.com>
2022-11-26 21:36:26 +0800

    Add files via upload

---
+++ webm.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+input=$1
+size=$2
+cpu=$3
+output=$4
+
+floatduration=$(ffprobe -i $input -show_entries format=duration -v quiet -of csv="p=0")
+duration=${floatduration%.*}
+
+while [ -z "$size" ] || [ -z "$input" ] || [ "$input" == "--help" ] || [ "$input" == "-help" ] ; do
+  echo "Help:
+Example 1: webm.sh video.mp4 5
+This will produce a roughly 5mb webm from the video.mp4 file, and output it to video.webm
+Example 2: webm.sh video.mp4 5 2 out.webm 
+This will produce a roughly 5mb webm from the video.mp4 file, use 2 cores to process the output, and name it out.webm"
+  exit 1
+done
+
+if [ -z "$cpu" ]
+then 
+	cpu=1
+fi
+
+if [ -z "$output" ]
+then 
+	output="${input%%.*}.webm"
+	echo $output
+fi
+
+
+ffmpeg -i $input -cpu-used $cpu -map 0:v -c libvpx-vp9 -pass 1 -f null -
+
+ffmpeg -i $input -cpu-used $cpu -map 0:v -map 0:a? -pass 2 -crf 30 -b:v $(($size*2**23 / $duration - 64*2**10)) $output
+
+rm -f ffmpeg2pass-0.log
